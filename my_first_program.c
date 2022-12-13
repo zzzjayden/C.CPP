fb6f4483e2d8d9fcb077411a890e2c54bf3ebe25
zzzjayden <perkman21@gmail.com>
2022-12-12 16:09:59 -0800

    update

--- my_first_program.c
+++ my_first_program.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include<cs50>
+#include<stdio.h>
+
 
 int main(void)
-{
-    printf("Hello World");
+{   string answer = get_string("what is your name?: ");
+    printf("Hello World\n");
+    printf("This is my first time coding in C\n");
+    printf("The goal is to learn everything about C programming before going back to college\n");
+    printf("Hello, %s\n" answer);
 }
